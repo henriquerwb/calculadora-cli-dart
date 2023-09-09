b7915420ac6c534306ae1df3246216b25df50ee3
henriquerwb <h3nriquegomes@gmail.com>
2023-09-08 21:26:46 -0300

    introduzido a utilização de funções

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -2,59 +2,93 @@ import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> arguments) {
-  while (true) {
-    print('Calculadora em Dart');
-    print('Escolha a opção desejada: ');
-    print('1 - Adição');
-    print('2 - Subtração');
-    print('3 - Multiplicação');
-    print('4 - Divisão');
-    print('5 - Sair');
+  print('Calculadora em Dart');
+  print('1 - Adição');
+  print('2 - Subtração');
+  print('3 - Multiplicação');
+  print('4 - Divisão');
+  print('5 - Sair');
 
-    var line = stdin.readLineSync(encoding: utf8);
-    var escolha = int.parse(line ?? "0");
+  var escolha = lerConsoleInt('Escolha a opção desejada:');
 
-    if (escolha == 5) {
-      print('Encerrando aplicação');
+  if (escolha == 5) {
+    print('Saindo do programa');
+  }
+
+  var num1 = lerConsoleDouble('Digite o primeiro número:');
+
+  var num2 = lerConsoleDouble('Digite o segundo número:');
+
+  calcular(escolha, num1, num2);
+}
+
+void calcular(int escolha, double num1, double num2) {
+  double resultado = 0;
+  switch (escolha) {
+    case 1:
+      resultado = somar(num1, num2);
       break;
-    }
+    case 2:
+      resultado = subtrair(num1, num2);
+      break;
+    case 3:
+      resultado = multiplicar(num1, num2);
+      break;
+    case 4:
+      resultado = dividir(num1, num2);
+      break;
+    case 5:
+      break;
+    default:
+      print("Opção inválida!");
+      exit(0);
+  }
+  print('Resultado da operação: $resultado');
+}
 
-    print('Digite o primeiro número: ');
-    line = stdin.readLineSync(encoding: utf8);
-    var num1 = double.parse(line ?? "0");
+double somar(double num1, double num2) {
+  return num1 + num2;
+}
 
-    print('Digite o segundo número: ');
-    line = stdin.readLineSync(encoding: utf8);
-    var num2 = double.parse(line ?? "0");
+double subtrair(double num1, double num2) {
+  return num1 - num2;
+}
 
-    double resultado = 0;
+double multiplicar(double num1, double num2) {
+  return num1 * num2;
+}
 
-    switch (escolha) {
-      case 1:
-        resultado = num1 + num2;
-        break;
-      case 2:
-        resultado = num1 - num2;
-        break;
-      case 3:
-        resultado = num1 * num2;
-        break;
-      case 4:
-        if (num2 != 0) {
-          resultado = num1 / num2;
-        } else {
-          print('Não é possível dividir por 0');
-          continue;
-        }
-        break;
-      case 5:
-        break;
-      default:
-        print('Opção inválida');
-        continue;
-    }
+double dividir(double num1, double num2) {
+  if (num2 == 0) {
+    print('Não é possível dividir por 0');
+    exit(0);
+  } else {
+    return num1 / num2;
+  }
+}
 
-    print('Resultado: $resultado');
-    break;
+String lerConsole(String texto) {
+  print(texto);
+  var line = stdin.readLineSync(encoding: utf8);
+  return line ?? "";
+}
+
+double lerConsoleDouble(String texto) {
+  var numero = double.tryParse(lerConsole(texto));
+  if (numero == null) {
+    print('Número inválido! Alternando para 0.');
+    return 0.0;
+  } else {
+    return numero;
+  }
+}
+
+int lerConsoleInt(String texto) {
+  var numero = int.tryParse(lerConsole(texto));
+  if (numero == null) {
+    print('Número inválido! Alternando para 0.');
+    return 5;
+  } else {
+    return numero;
   }
 }
